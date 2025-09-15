6e607b7787644d3b72663066eb9f4765e2a5ae2b
Preethampree <lakshmipreetham17@gmail.com>
2025-09-15 11:55:03 +0530

    Task-5 Completed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import session from 'express-session';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
       },
     }),
   );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   
   await app.listen(process.env.PORT ?? 3000);
 }
